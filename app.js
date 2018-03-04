e2e0cffbc070271a2ea4a4bc416c4cddc9f2431a
rajesh7277 <36913852+rajesh7277@users.noreply.github.com>
2018-03-04 12:33:12 +0530

    update in .env, CountryMaster.html
    1-change 's the port number and path of the env
    2- integration in country master and change in controller 'countryMasterCtrl
    3- working in pagination to fetching the data from database.
    4- changes in the app.js file for loading the all countryMaster.js, stateMaster.js.... etc file are loaded in the aap.js file 
    5- aap.js file  added the 'api /'

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 const express = require('express');
-const path = require('path');
+
 const fs = require('fs');
-const favicon = require('serve-favicon');
+//const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 var cons = require('consolidate');
 const logger = require('./logger');
 const config = require('./model/config');
 const index = require('./routes/index');
-const country = require('./routes/master/country_master');
-const state = require('./routes/master/state_master');
-
-
+var router=express.Router();
+// changes by rajesh(t0007)
+var appRoutes = require('./routes/master/country_master')(router);
+var appRoutes = require('./routes/master/state_master')(router);
+var appRoutes=require('./routes/master/user_master')(router);
+//end of rajesh(t0007)
 var app = express();
  
+///////////pavithra  //////
+
+require('dotenv').config();
+  var path = require('./bin/dev');
+
+///////////////////end pavithra//////
+
+var path1 = require('path');
 // view engine setup
 app.engine('html', cons.swig)
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path1.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path1.join(__dirname, 'public')));
+app.use('/api',appRoutes);
 app.use('/', index);
-app.use('/country', country);
-app.use('/state', state);
+//app.use('/country', country);
+//app.use('/state', state);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
